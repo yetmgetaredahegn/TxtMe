271cc89ebb28fe9adde1f07d5b43c632f82d1c67
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-09-08 16:12:45 +0300

    working with consumers and adding connect,disconnect and tester_message

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,7 +1,36 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
-
+import json
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        return await super().connect()
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f"chat_{self.room_name}"
+        
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'tester_message',
+                'tester': 'tester',
+            }
+        )
+
+
+    async def tester_message(self, event):
+        tester = event['tester']
+
+        await self.send(text_data=json.dumps({
+            'tester': tester
+        }))
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+        
     pass
